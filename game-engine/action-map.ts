cf5e8c4984ccabe9d056eba7123504e06bbfa969
chaehaejoong <svneh0329@naver.com>
2026-07-20 01:10:58 +0900

    rename actions.ts to action-map.ts & add real actions.ts

---
+++ game-engine/action-map.ts
@@ -0,0 +1,65 @@
+import { coupOnSuccess, foreignAidOnSuccess, incomeOnSuccess, taxOnSuccess } from "./actions.js";
+import {
+  Card,
+  ActionType,
+  type GameState,
+} from "./types.js";
+
+export type ActionInfo = {
+  blockableCard: Card[];
+  blockerScope: "anyone" | "target" | "none";
+  claimedCard: Card | null;
+  isTargetRequired: boolean;
+  onSuccess: (state: GameState, targetId?: number) => void;
+};
+export type ActionMap = Record<ActionType, ActionInfo>;
+
+export const actionMap: ActionMap = {
+  [ActionType.INCOME]: {
+    blockableCard: [],
+    blockerScope: "none",
+    claimedCard: null,
+    isTargetRequired: false,
+    onSuccess: incomeOnSuccess
+  },
+  [ActionType.FOREIGN_AID]: {
+    blockableCard: [Card.DUKE],
+    blockerScope: "anyone",
+    claimedCard: null,
+    isTargetRequired: false,
+    onSuccess: foreignAidOnSuccess
+  },
+  [ActionType.COUP]: {
+    blockableCard: [],
+    blockerScope: "none",
+    claimedCard: null,
+    isTargetRequired: true,
+    onSuccess: coupOnSuccess
+  },
+  [ActionType.TAX]: {
+    blockableCard: [],
+    blockerScope: "none",
+    claimedCard: Card.DUKE,
+    isTargetRequired: false,
+    onSuccess: taxOnSuccess
+  },
+  [ActionType.STEAL]: {
+    blockableCard: [Card.AMBASSADOR, Card.CAPTAIN],
+    blockerScope: "target",
+    claimedCard: Card.CAPTAIN,
+    isTargetRequired: true,
+    onSuccess: 
+  },
+  [ActionType.CHANGE]: {
+    blockableCard: [],
+    blockerScope: "none",
+    claimedCard: Card.AMBASSADOR,
+    isTargetRequired: false,
+  },
+  [ActionType.ASSASSINATE]: {
+    blockableCard: [Card.CONTESSA],
+    blockerScope: "target",
+    claimedCard: Card.ASSASSIN,
+    isTargetRequired: true,
+  },
+};
--- game-engine/actions.ts
+++ game-engine/actions.ts
@@ -1,79 +1,27 @@
-import {
-  Card,
-  ActionType,
-  type GameState,
-  type Player,
-  Phase,
-  DecisionType,
-} from "./types.js";
+import { DecisionType, Phase, type GameState } from "./types.js";
 
-export type ActionInfo = {
-  blockableCard: Card[];
-  blockerScope: "anyone" | "target" | "none";
-  claimedCard: Card | null;
-  isTargetRequired: boolean;
-  onSuccess: (state: GameState, targetId?: number) => void;
-};
-export type ActionMap = Record<ActionType, ActionInfo>;
+export function incomeOnSuccess(state: GameState): void {
+  state.turnPlayer.coin += 1;
+}
 
-export const actionMap: ActionMap = {
-  [ActionType.INCOME]: {
-    blockableCard: [],
-    blockerScope: "none",
-    claimedCard: null,
-    isTargetRequired: false,
-    onSuccess: (state) => {
-      state.turnPlayer.coin += 1;
-    },
-  },
-  [ActionType.FOREIGN_AID]: {
-    blockableCard: [Card.DUKE],
-    blockerScope: "anyone",
-    claimedCard: null,
-    isTargetRequired: false,
-    onSuccess: (state) => {
-      state.turnPlayer.coin += 2;
-    },
-  },
-  [ActionType.COUP]: {
-    blockableCard: [],
-    blockerScope: "none",
-    claimedCard: null,
-    isTargetRequired: true,
-    onSuccess: (state, targetId) => {
-      state.turnPlayer.coin -= 7;
-      state.phase = Phase.AWAIT_DECISION;
+export function foreignAidOnSuccess(state: GameState): void {
+  state.turnPlayer.coin += 2;
+}
 
-      if (!targetId) throw new Error("need targetId");
+export function coupOnSuccess(state: GameState, targetId?: number): void {
+  state.turnPlayer.coin -= 7;
+  state.phase = Phase.AWAIT_DECISION;
 
-      state.pendingDecision = {
-        type: DecisionType.LOSE_CARD,
-        playerId: targetId,
-      };
-    },
-  },
-  [ActionType.TAX]: {
-    blockableCard: [],
-    blockerScope: "none",
-    claimedCard: Card.DUKE,
-    isTargetRequired: false,
-  },
-  [ActionType.STEAL]: {
-    blockableCard: [Card.AMBASSADOR, Card.CAPTAIN],
-    blockerScope: "target",
-    claimedCard: Card.CAPTAIN,
-    isTargetRequired: true,
-  },
-  [ActionType.CHANGE]: {
-    blockableCard: [],
-    blockerScope: "none",
-    claimedCard: Card.AMBASSADOR,
-    isTargetRequired: false,
-  },
-  [ActionType.ASSASSINATE]: {
-    blockableCard: [Card.CONTESSA],
-    blockerScope: "target",
-    claimedCard: Card.ASSASSIN,
-    isTargetRequired: true,
-  },
-};
+  if (!targetId) throw new Error("need targetId");
+
+  state.pendingDecision = {
+    type: DecisionType.LOSE_CARD,
+    playerId: targetId,
+  };
+}
+
+export function taxOnSuccess(state: GameState): void {
+  state.turnPlayer.coin += 3;
+}
+
+export function stealOnSuccess(state: GameState,)
